c450ad2f59d8df59e5b1653ba906ac991bce3b2d
saubhagyadixit <47620983+saubhagyadixit@users.noreply.github.com>
2020-04-01 14:59:45 +0530

    Update 8.5 Lists.py
    error

--- Coursera Programming for Everybody (Python)/8.5 Lists.py
+++ Coursera Programming for Everybody (Python)/8.5 Lists.py
@@ -15,8 +15,7 @@ for line in fh :
     line = line.rstrip()
     if not line.startswith('From '): continue        
     words = line.split()
-    print words[1]
+    print (words[1])
     counter +=1
 
 print "There were", counter, "lines in the file with From as the first word"
-
